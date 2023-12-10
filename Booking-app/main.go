5cff88084607a8b1158f957553f2b3a789533974
Zoti <matheus.a.zoti@gmail.com>
2023-12-10 13:49:04 -0300

    Data types and Pointers
    Using specific data types and basic pointers syntax

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -3,17 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = conferenceTickets
-
-	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available,", conferenceTickets, remainingTickets)
-
-
 	var userName string
+	var userTickets int
+	conferenceName := "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = conferenceTickets
+
+
+	//bloco principal
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	//descobrir o tipo usando %T no printf()
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
 
 	// ask user for their name
-	userName = "Tom"
-	fmt.Println(userName)
+	fmt.Printf("What is your name?\n")
+	fmt.Scan(&userName)
+
+
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 }
