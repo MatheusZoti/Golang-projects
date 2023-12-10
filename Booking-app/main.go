9587b1bed566ab4c2d88b3b1be451813a5a8f4ad
Zoti <matheus.a.zoti@gmail.com>
2023-12-10 15:18:47 -0300

    Inputs, Array e Slices
    Here I have defined some basic concepts of Inputs, Array e Slices

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -2,26 +2,39 @@ package main
 
 import "fmt"
 
+
+
 func main() {
-	var userName string
-	var userTickets int
+	bookings := []string{}
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
 	conferenceName := "Go Conference"
 	const conferenceTickets uint = 50
 	var remainingTickets uint = conferenceTickets
 
 
-	//bloco principal
+	//Mensagem inicial e contagem dos tickets disponiveis
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	//descobrir o tipo usando %T no printf()
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
+	// Informações do usuário e compra dos tickets
+	fmt.Printf("What is your first name?\n")
+	fmt.Scan(&firstName)
+	fmt.Printf("What is your last name?\n")
+	fmt.Scan(&lastName)
+	fmt.Printf("What is your email address:\n")
+	fmt.Scan(&email)
+	fmt.Printf("How many tickets do you want?\n")
+	fmt.Scan(&userTickets)
 
-	// ask user for their name
-	fmt.Printf("What is your name?\n")
-	fmt.Scan(&userName)
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName + " " + lastName)
 
+	fmt.Printf("Thank you %v %v for booking %v tickets. You'll receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Printf("Essas são todas as pessoas que compraram nossos tickets: %v\n", bookings)
 }
