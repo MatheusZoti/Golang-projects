4da5a6d45813eb5a9c2b028fa53b4dccaa227a7f
Zoti <matheus.a.zoti@gmail.com>
2023-12-10 21:02:12 -0300

    End of day1
    1:36
    https://www.youtube.com/watch?v=yyUHQIec83I

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -1,40 +1,67 @@
 package main
 
-import "fmt"
-
-
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	bookings := []string{}
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-
 	conferenceName := "Go Conference"
 	const conferenceTickets uint = 50
 	var remainingTickets uint = conferenceTickets
+	bookings := []string{}
 
 
 	//Mensagem inicial e contagem dos tickets disponiveis
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 
-	// Informações do usuário e compra dos tickets
-	fmt.Printf("What is your first name?\n")
-	fmt.Scan(&firstName)
-	fmt.Printf("What is your last name?\n")
-	fmt.Scan(&lastName)
-	fmt.Printf("What is your email address:\n")
-	fmt.Scan(&email)
-	fmt.Printf("How many tickets do you want?\n")
-	fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
+	//For LOOP
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		
+		// Informações do usuário e compra dos tickets
+		fmt.Printf("What is your first name?\n")
+		fmt.Scan(&firstName)
+		fmt.Printf("What is your last name?\n")
+		fmt.Scan(&lastName)
+		fmt.Printf("What is your email address:\n")
+		fmt.Scan(&email)
+		fmt.Printf("How many tickets do you want?\n")
+		fmt.Scan(&userTickets)
+		if userTickets > remainingTickets {
+			fmt.Printf("Sorry, we only have %v tickets available.\n", remainingTickets)
+			continue 
+		}
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You'll receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		// Atualizar as variaveis: Tickets que sobraram; Array dos compradores
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName + " " + lastName)
+	
+		// Mensagem de Obrigado pela compra E quantos tickets sobraram após a mesma
+		fmt.Printf("Thank you %v %v for booking %v tickets. You'll receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	
 
-	fmt.Printf("Essas são todas as pessoas que compraram nossos tickets: %v\n", bookings)
+
+		firstNames := []string{}
+		for _, bookingValue := range bookings {
+			 var names = strings.Fields(bookingValue)
+			 firstNames = append(firstNames, names[0])
+		}
+
+
+		// Lista de pessoas que compraram o ticket
+		fmt.Printf("Esses são os nomes dos que compraram nossos tickets: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// fechar a aplicação
+			fmt.Printf("Our conference is booked out. Come back next year.")
+			break
+		}
+	}
 }
