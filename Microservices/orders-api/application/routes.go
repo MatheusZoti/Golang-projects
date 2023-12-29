b426c8e4988c2e793eb20fb25aed5cbab613b40b
Zoti <matheus.a.zoti@gmail.com>
2023-12-28 22:08:23 -0300

    Need to finish this step
    1 - "dicas" created
    
    2 - Beginning of the project:
          - CRUD app for storing a data type
    
    3 - Start Restructuring the file system to make the project cleaner
    
    video? https://www.youtube.com/watch?v=N9LhKjPibuU&list=PL4cUxeGkcC9iImF8w9FbFOc2UntutL9Wv&index=3

---
+++ Microservices/orders-api/application/routes.go
@@ -0,0 +1,14 @@
+package application
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func loadRoutes() *chi.Mux {
+	router := chi.NewHandler()
+
+	router.Use(middleware.Logger)
+}
